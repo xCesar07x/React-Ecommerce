0346c7e7e29a8fdf1d56eaac59311dbf10a5a8ba
xCesar07x <mathcesar02@gmail.com>
2023-02-02 23:02:39 -0500

    add-button-cart

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -29,16 +29,16 @@ const Home = () => {
 
 
 
-    // const [addCart, setAddCart] = useState(1)
+    const [addCart, setAddCart] = useState(1)
 
 
-    // const addToCart = () => {
-    //     const cart = {
-    //         quantity: addCart,
-    //         productId: id
-    //     }
-    //     dispatch(addCartThunk(cart))
-    // }
+    const addToCart = (quantity, productId) => {
+        const cart = {
+            quantity: 1,
+            productId: productId
+        }
+        dispatch(addCartThunk(cart))
+    }
 
 
 
@@ -209,7 +209,8 @@ const Home = () => {
                                             </Card.Body>
                                         </div>
 
-                                        <Button style={{ width: "100%" }} variant="primary">
+                                        <Button onClick={() =>  addToCart(1, product.id) }
+                                            style={{ width: "100%" }} variant="primary">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="whitesmoke" className="bi bi-cart" viewBox="0 0 16 16"> <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" /> </svg>
                                         </Button>
                                     </Card>
--- src/pages/ProductId.jsx
+++ src/pages/ProductId.jsx
@@ -39,10 +39,10 @@ const ProductId = () => {
     const [addCart, setAddCart] = useState(1)
 
 
-    const addToCart = () => {
+    const addToCart = (quantity, productId) => {
         const cart = {
-            quantity: addCart,
-            productId: products.id
+            quantity: quantity,
+            productId: productId
         }
         dispatch(addCartThunk(cart))
     }
@@ -172,7 +172,7 @@ const ProductId = () => {
 
 
                     <div style={{ width: "100" }}>
-                        <Button onClick={addToCart} style={{ display: "flex", alignItems: "center", gap: "1rem", width: "100%", justifyContent: "center" }} variant="primary">
+                        <Button onClick={() => addToCart(addCart, products.id)} style={{ display: "flex", alignItems: "center", gap: "1rem", width: "100%", justifyContent: "center" }} variant="primary">
 
                             <p style={{ margin: "0" }}>
                                 Add to store
@@ -242,7 +242,7 @@ const ProductId = () => {
                                 </Card.Body>
                             </Card>
                             <Button 
-                                style={{ width: "100%" }} variant="primary">
+                                style={{ width: "100%" }} variant="primary" onClick={() => addToCart(1, product.id)}>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="whitesmoke" className="bi bi-cart" viewBox="0 0 16 16"> <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" /> </svg>
                             </Button>
                         </div>
